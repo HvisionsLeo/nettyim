ea072fb88ee7085a31be09bc7a10e946d1ebb613
Leo <jisanli536@163.com>
2018-11-28 17:54:50 +0800

    客户端

--- src/main/java/com/leo/netty/NettyClient.java
+++ src/main/java/com/leo/netty/NettyClient.java
@@ -4,13 +4,14 @@ package com.leo.netty;
 import io.netty.bootstrap.Bootstrap;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioSocketChannel;
-import io.netty.handler.codec.string.StringDecoder;
 import io.netty.handler.codec.string.StringEncoder;
 import org.apache.commons.lang3.time.DateFormatUtils;
 
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @Description: netty客户端
@@ -18,26 +19,49 @@ import java.util.Date;
  * @Date: 2018-11-28 下午 12:13
  */
 public class NettyClient {
+    private static int MAX_RETRY = 5;
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
+
         Bootstrap bootstrap = new Bootstrap();
         NioEventLoopGroup group = new NioEventLoopGroup();
 
-        bootstrap.group(group)
+        bootstrap
+                // 指定线程魔性
+                .group(group)
+                // 指定IO类型为NIO
                 .channel(NioSocketChannel.class)
+                // 设置TCP底层属性
+                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5000) // 超时时间
+                .option(ChannelOption.SO_KEEPALIVE, true)   // 心跳机制
+                .option(ChannelOption.TCP_NODELAY, true)
+                // IO 处理逻辑
                 .handler(new ChannelInitializer<Channel>() {
                     @Override
                     protected void initChannel(Channel channel) throws Exception {
                         channel.pipeline().addLast(new StringEncoder());
                     }
                 });
-        Channel channel = bootstrap.connect("127.0.0.1", 8888).channel();
-        while (true) {
-            String s = DateFormatUtils.format(new Date(), "yyyy-MM-dd HH:mm:ss") + ":Hello World!";
-            channel.writeAndFlush(s);
-            System.out.println("写入：" + s);
-            Thread.sleep(2000);
-        }
+        // 建立连接
+        connect(bootstrap, "127.0.0.1", 8081, MAX_RETRY);
+    }
 
+    private static void connect(Bootstrap bootstrap, String host, int port, int retry) {
+        bootstrap.connect(host, port).addListener(future -> {
+            if (future.isSuccess()) {
+                System.out.println("连接成功！");
+            } else if (retry == 0) {
+                System.err.println(DateFormatUtils.format(new Date(), "yyyy-MM-dd HH:mm:ss") + "重试次数用完，放弃连接。。。");
+            } else {
+                // 第几次重连
+                int order = (MAX_RETRY - retry) + 1;
+                // 本次重连间隔
+                int delay = 1 << order;
+                System.err.println(DateFormatUtils.format(new Date(), "yyyy-MM-dd HH:mm:ss") + "连接失败，第" + order + "次重连。。。");
+                bootstrap.config().group().schedule(() ->
+                        connect(bootstrap, host, port, retry - 1), delay, TimeUnit.SECONDS);
+
+            }
+        });
     }
 }
--- src/main/java/com/leo/netty/NettyServer.java
+++ src/main/java/com/leo/netty/NettyServer.java
@@ -3,6 +3,7 @@ package com.leo.netty;
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
@@ -23,8 +24,24 @@ public class NettyServer {
         // 对应IOServer中的负责读取数据的线程，主要用于读取数据以及业务逻辑处理
         NioEventLoopGroup workGroup = new NioEventLoopGroup();
 
-        serverBootstrap.group(bossGroup, workGroup)
+        serverBootstrap
+                // 给引导类配置两大线程组
+                .group(bossGroup, workGroup)
+                // 指定服务端的 IO 模型为NIO,若为BIO则配置OioServerSocketChannel.class
                 .channel(NioServerSocketChannel.class)
+                // 给每条连接设置一些TCP底层相关的属性
+                .childOption(ChannelOption.SO_KEEPALIVE, true)  // 表示是否开启TCP底层心跳机制，true为开启
+                .childOption(ChannelOption.TCP_NODELAY, true)   // 表示是否开启Nagle算法，true表示关闭，false表示开启，通俗地说，如果要求高实时性，有数据发送时就马上发送，就关闭，如果需要减少发送次数减少网络交互，就开启。
+                // 给服务端设置一些属性
+                .option(ChannelOption.SO_BACKLOG, 1024)
+                // 用于指定在服务端启动过程中的一些逻辑,通常用不到
+                .handler(new ChannelInitializer<NioServerSocketChannel>() {
+                    @Override
+                    protected void initChannel(NioServerSocketChannel ch) throws Exception {
+                        System.out.println("服务端启动中。。。");
+                    }
+                })
+                // 业务处理逻辑
                 .childHandler(new ChannelInitializer<NioSocketChannel>() {
                     @Override
                     protected void initChannel(NioSocketChannel nioSocketChannel) throws Exception {
@@ -36,7 +53,20 @@ public class NettyServer {
                             }
                         });
                     }
-                })
-                .bind(8888);
+                });
+        bind(serverBootstrap, 8081);
+    }
+
+    private static void bind(final ServerBootstrap serverBootstrap, final int port) {
+        serverBootstrap.bind(port).addListener(future -> {
+            // 判断是否绑定成功
+            if (future.isSuccess()) {
+                System.out.println("端口号：" + port + "绑定成功！");
+            } else {
+                // 绑定失败，则将端口号+1继续绑定
+                System.err.println("端口号：" + port + "绑定失败！");
+                bind(serverBootstrap, port + 1);
+            }
+        });
     }
 }
